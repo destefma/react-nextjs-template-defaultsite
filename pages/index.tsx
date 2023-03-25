f4ab30844c2f1199077c518a6625bd11e18870db
Manuel De Stefani <41085653+destefma@users.noreply.github.com>
2023-03-25 21:39:58 +0100

    Add files via upload
    Add template Index File

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
-import { SocialIcon } from "react-social-icons";
-import AnimatedImage from "../src/components/Animation/AnimatedImage";
-import HeroSwipper from "../src/components/Heros/HeroSwipper";
-import InfoNavBar from "../src/components/NavBar/InfoNavBar";
 import NavBar from "../src/components/NavBar/NavBar";
 import NavBarItem from "../src/components/NavBar/NavBarItems/NavBarItem";
 import SideBar from "../src/components/NavBar/SideBar";
-import Features from "../src/layouts/gastro/Features";
-import GastroEvents from "../src/layouts/gastro/GastroEvents";
-import GastroMenu from "../src/layouts/gastro/GastroMenu";
-import GastroInnovation from "../src/layouts/gastro/GastroInnovation";
-import PhoneReservationTable from "../src/layouts/gastro/PhoneReservationTable";
 import useScreenSize from "../src/hooks/useScreenSize";
-import TeamSection from "../src/layouts/gastro/TeamSection";
-import WelcomeSection from "../src/layouts/gastro/WelcomeSection";
 import Footer from "../src/layouts/main/Footer";
-import Gallery from "../src/layouts/main/Gallery";
 
-import { GastroNavBarItemProps } from "../src/constans/ComponentDefaultProps";
+import { DashboardNavBarItemProps } from "../src/constans/ComponentDefaultProps";
 
-NavBarItem.defaultProps = GastroNavBarItemProps;
+NavBarItem.defaultProps = DashboardNavBarItemProps;
 
 const GastroPage: NextPage = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -31,16 +17,15 @@ const GastroPage: NextPage = () => {
   return (
     <div className="scroll-smooth">
       <Head>
-        <title>Gastro</title>
+        <title>Template</title>
         <meta
           name="description"
-          content="Gastro Website - Designed by DeStefani"
+          content="Template Website - Designed by DeStefani"
         />
         <meta
           name="keywords"
-          content="Eisdiele, Eisdiele mit leckerm Cafê, Eisdiele mit BubbleWaffel, Eisdiele mit Bubble Tee, Eisdiele mit leckerem Eis, Eiscafe, Eisdiele"
+          content="Template, default, Website"
         />
-        <link rel="icon" href="/assets/venezia2_logo-removebg.webp"></link>
       </Head>
 
       <NavBar
@@ -58,20 +43,6 @@ const GastroPage: NextPage = () => {
         <NavBarItem href="#welcome-section" className="w-auto">
           Über uns
         </NavBarItem>
-        {useScreenSize().width >= 1280 ? (
-          <div className="z-50">
-            <Link href="/">
-              <Image
-                src={"./assets/venezia2_logo-removebg.webp"}
-                alt="Overflowing Website Logo Image"
-                loading="lazy"
-                width={50}
-                height={50}
-                className="w-20 h-20 inset-y-0 rounded-full hover:scale-150 z-50"
-              />
-            </Link>
-          </div>
-        ) : null}
         <NavBarItem href="#gallery" className="w-auto">
           Gallery
         </NavBarItem>
@@ -105,20 +76,7 @@ const GastroPage: NextPage = () => {
       </SideBar>
 
       <main>
-        <InfoNavBar className="md:hidden" telNumber="+49 111 111 111 11" />
-        {useScreenSize().width >= 1280 ? <AnimatedImage /> : null}
-        <HeroSwipper />
-        <WelcomeSection bgColor="primary" />
-        <GastroInnovation />
-        <GastroMenu />
-        <Features id="range" />
-        <TeamSection />
-        <Gallery />
-        <PhoneReservationTable
-          bgColor="bg-gray-100"
-          telNumber="+49 111 111 111 11"
-          id="reservieren"
-        />
+      
         <Footer />
       </main>
     </div>
